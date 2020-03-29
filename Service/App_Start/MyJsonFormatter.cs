1777a4d57b3d157ae39ad21cd725cc55f4ca5c23
Daniel <daniellegaiec20@gmail.com>
2020-03-29 15:25:51 +0200

    Add project files.

---
+++ Service/App_Start/MyJsonFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+
+public class MyJsonFormatter : JsonMediaTypeFormatter {
+
+    public MyJsonFormatter() {
+        this.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+        this.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+    }
+
+    public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType) {
+        base.SetDefaultContentHeaders(type, headers, mediaType);
+        headers.ContentType = new MediaTypeHeaderValue("application/json");
+    }
+}

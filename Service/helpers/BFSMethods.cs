9e9fda687acd98d45b7b9e1fc090b8b04b0de3df
ChuncI-Iunmaru <pwalczak796@gmail.com>
2020-06-24 15:36:51 +0200

    Dodaj metody liczące przy użyciu BFS

---
+++ Service/helpers/BFSMethods.cs
@@ -0,0 +1,92 @@
+﻿using Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Service.helpers
+{
+    public class BFSMethods
+    {
+
+        public static Dictionary<string, HashSet<string>> graphAsAdjacencyList(Graph g)
+        {
+            Dictionary<string, HashSet<string>> result = new Dictionary<string, HashSet<string>>();
+            foreach (GraphPart p in g.GraphPart){
+                HashSet<string> adjacent = new HashSet<string>();
+                foreach (Edge e in p.Edge)
+                {
+                    adjacent.Add(e.Destination.Label);
+                }
+                result.Add(p.Node.Label, adjacent);
+            }
+            return result;
+        }
+
+        private static Func<string, IEnumerable<string>> ShortestPathFunction(Dictionary<string, HashSet<string>> adjList, string start)
+        {
+            var previous = new Dictionary<string, string>();
+
+            var queue = new Queue<string>();
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+                foreach (var neighbor in adjList[vertex])
+                {
+                    if (previous.ContainsKey(neighbor))
+                        continue;
+
+                    previous[neighbor] = vertex;
+                    queue.Enqueue(neighbor);
+                }
+            }
+
+            Func<string, IEnumerable<string>> shortestPath = v => {
+                var path = new List<string> { };
+
+                var current = v;
+                while (!current.Equals(start))
+                {
+                    if (previous.ContainsKey(current))
+                    {
+                        path.Add(current);
+                        current = previous[current];
+                    } else
+                    {
+                        // Coś poszło nie tak, zakładam że wierzochłek v jest nieosiągalny z danego startowego?
+                        return path;
+                    }
+                };
+
+                path.Add(start);
+                path.Reverse();
+
+                return path;
+            };
+
+            return shortestPath;
+        }
+
+        public static Dictionary<string, double> shortestPathToAll(Graph graph, Node start)
+        {
+            Dictionary<string, double> result = new Dictionary<string, double>();
+            var adjList = graphAsAdjacencyList(graph);
+            var shortestPath = ShortestPathFunction(adjList, start.Label);
+            foreach(string vertex in adjList.Keys)
+            {
+                // Graf nieskierowany - nie musi istnieć ścieżka do każdego z pozostałtych w tym kierunku
+                Console.WriteLine("shortest path to {0,2}: {1}",vertex, string.Join(", ", shortestPath(vertex)));
+                result.Add(vertex, shortestPath(vertex).Count()-1);
+            }
+            return result;
+        }
+
+        public static double shortestPathToGiven(Graph graph, Node start, Node end)
+        {
+            var adjList = graphAsAdjacencyList(graph);
+            var shortestPath = ShortestPathFunction(adjList, start.Label);
+            return shortestPath(end.Label).Count() - 1;
+        }
+    }
+}
---
+++ ServiceTest/BFSTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.Core.Internal;
+using Moq;
+using NUnit.Framework;
+using Service.helpers;
+using Service.Models;
+using Service.Services;
+using Service.Services.Interfaces;
+using static Service.helpers.BFSMethods;
+
+namespace ServiceTest
+{
+    [TestFixture]
+    class BFSTests
+    {
+        private IGraphService _graphService;
+        private FakeDb<Graph> _fakeDb;
+
+        [SetUp]
+        public void Init()
+        {
+            _fakeDb = new FakeDb<Graph>();
+            var myDbContextMock = new Mock<MyDbContext>();
+            _graphService = new GraphService();
+        }
+
+        [Test]
+        public void TestBFS()
+        {
+            var graph = _graphService.NewGraph(true);
+            var nodeA = _graphService.AddNode(ref graph, "nodeA");
+            var nodeB = _graphService.AddNode(ref graph, "nodeB");
+            var nodeC = _graphService.AddNode(ref graph, "nodeC");
+            _graphService.AddEdge(nodeA, nodeB, ref graph, 10.0D);
+            _graphService.AddEdge(nodeB, nodeA, ref graph, 12.0D);
+            _graphService.AddEdge(nodeC, nodeA, ref graph, 5.0D);
+
+            Dictionary<string, HashSet<string>> adjList = graphAsAdjacencyList(graph);
+            foreach (KeyValuePair<string, HashSet<string>> kpv in adjList)
+            {
+                Console.WriteLine(kpv.Key + ": ");
+                foreach (string s in kpv.Value)
+                {
+                    Console.Write(s + ", ");
+                }
+                Console.WriteLine();
+            }
+            var result = shortestPathToAll(graph, nodeA);
+            Console.WriteLine("Od {0}", nodeA.Label);
+            foreach (KeyValuePair<string, double> kpv in result)
+            {
+                Console.WriteLine("do {0} - długość {1}", kpv.Key, kpv.Value);
+            }
+        }
+    }
+}
